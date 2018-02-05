0febe4347a044bbdf6c87b79ca851609a1e5dab9
Kathy Hudson <khitx@icloud.com>
2018-02-05 18:21:05 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,7 +8,12 @@ def create_project_hash
 
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {}
+    projects[title.to_sym] = {
+      image_link: project.css("div.project-thumbnail a img").attribute("src").value,
+      description: project.css("p.bbcard_blurb").text,
+      location: project.css("ul.project-meta span.location-name").text,
+      percent_funded: project.css("ul.project-stats .fist.funded strong").text.gsub("%", "").to_i      
+    }
   end
 
   projects
@@ -16,12 +21,6 @@ end
 
 create_project_hash
 
-# projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-# location: project.css("ul.project-meta span.location-name").text
-# percent_funded: project.css("ul.project-stats .fist.funded strong").text.gsub("%", "").to_i
 
 #:projects => {
 #  "My Great Project"  => {
